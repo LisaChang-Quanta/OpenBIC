689c9a6f2da1ea29b92024a259ccec0748881153
lisa <Lisa-Chang@quantatw.com>
2024-11-26 09:40:52 +0800

    fab2 312M clock test
    fab2 eusb repeater test

--- meta-facebook/minerva-ag/src/platform/plat_isr.c
+++ meta-facebook/minerva-ag/src/platform/plat_isr.c
@@ -32,6 +32,13 @@ extern uint8_t ina230_init(sensor_cfg *cfg);
 
 LOG_MODULE_REGISTER(plat_isr);
 
+#define AEGIS_CPLD_ADDR (0x4C >> 1)
+#define AEGIS_312M_CLK_GEN_ADDR (0x12 >> 1)
+#define AEGIS_EUSB_REPEATER_ADDR (0x86 >> 1)
+void check_clk_handler();
+
+K_WORK_DELAYABLE_DEFINE(check_clk_work, check_clk_handler);
+
 void ISR_GPIO_FM_ASIC_0_THERMTRIP_R_N()
 {
 	LOG_INF("gpio_%d_isr called, val=%d , dir= %d", FM_ASIC_0_THERMTRIP_R_N,
@@ -82,4 +89,145 @@ void ISR_GPIO_FM_PLD_UBC_EN_R()
 {
 	LOG_INF("gpio_%d_isr called, val=%d , dir= %d", FM_PLD_UBC_EN_R, gpio_get(FM_PLD_UBC_EN_R),
 		gpio_get_direction(FM_PLD_UBC_EN_R));
+	plat_clock_init();
+}
+
+bool cpld_i2c_read(uint8_t bus, uint8_t addr, uint8_t offset, uint8_t *data, uint8_t len)
+{
+	CHECK_NULL_ARG_WITH_RETURN(data, false);
+
+	memset(data, 0, len);
+
+	I2C_MSG i2c_msg = { 0 };
+	uint8_t retry = 5;
+	i2c_msg.bus = bus;
+	i2c_msg.target_addr = addr;
+	i2c_msg.tx_len = 1;
+	i2c_msg.rx_len = len;
+	i2c_msg.data[0] = offset;
+
+	if (i2c_master_read(&i2c_msg, retry)) {
+		LOG_ERR("Failed to read reg, bus: %d, addr: 0x%x, reg: 0x%x", bus, addr, offset);
+		return false;
+	}
+
+	memcpy(data, i2c_msg.data, len);
+	return true;
+}
+
+bool cpld_i2c_write(uint8_t bus, uint8_t addr, uint8_t offset, uint8_t *data, uint8_t len)
+{
+	CHECK_NULL_ARG_WITH_RETURN(data, false);
+
+	I2C_MSG i2c_msg = { 0 };
+	uint8_t retry = 5;
+	i2c_msg.bus = bus;
+	i2c_msg.target_addr = addr;
+	i2c_msg.tx_len = len + 1;
+	i2c_msg.data[0] = offset;
+
+	if (len > 0)
+		memcpy(&i2c_msg.data[1], data, len);
+
+	if (i2c_master_write(&i2c_msg, retry)) {
+		LOG_ERR("Failed to write reg, bus: %d, addr: 0x%x, reg: 0x%x", bus, addr, offset);
+		return false;
+	}
+	return true;
+}
+
+void check_clk_handler()
+{
+	uint8_t data[4] = { 0 };
+
+	if (!cpld_i2c_read(I2C_BUS5, AEGIS_CPLD_ADDR, 0x0B, data, 1)) {
+		LOG_ERR("Failed to read cpld");
+		return;
+	}
+
+	uint8_t pwrgd_p3v3_and_p1v8 = (data[0] & GENMASK(4, 3)) >> 3;
+	if (pwrgd_p3v3_and_p1v8 == 3) {
+		memset(data, 0, sizeof(data));
+		memcpy(data, (uint8_t[]){ 0x00, 0x01, 0x00, 0x00 }, 4);
+		if (!cpld_i2c_write(I2C_BUS1, AEGIS_312M_CLK_GEN_ADDR, 0xFC, data, 4)) {
+			LOG_ERR("Failed to write 312M CLK GEN");
+			return;
+		}
+		if (!cpld_i2c_read(I2C_BUS1, AEGIS_312M_CLK_GEN_ADDR, 0x04, data, 1)) {
+			LOG_ERR("Failed to read 312M CLK GEN");
+			return;
+		}
+		memset(data, 0, sizeof(data));
+		memcpy(data, (uint8_t[]){ 0x84 }, 1);
+		if (!cpld_i2c_write(I2C_BUS1, AEGIS_312M_CLK_GEN_ADDR, 0x04, data, 1)) {
+			LOG_ERR("Failed to write 312M CLK GEN");
+			return;
+		}
+		if (!cpld_i2c_read(I2C_BUS1, AEGIS_312M_CLK_GEN_ADDR, 0x04, data, 1)) {
+			LOG_ERR("Failed to read 312M CLK GEN");
+			return;
+		}
+		memset(data, 0, sizeof(data));
+		memcpy(data, (uint8_t[]){ 0x00, 0x01, 0x00, 0x00 }, 4);
+		if (!cpld_i2c_write(I2C_BUS1, AEGIS_312M_CLK_GEN_ADDR, 0xFC, data, 4)) {
+			LOG_ERR("Failed to write 312M CLK GEN");
+			return;
+		}
+		if (!cpld_i2c_read(I2C_BUS1, AEGIS_312M_CLK_GEN_ADDR, 0x14, data, 1)) {
+			LOG_ERR("Failed to read 312M CLK GEN");
+			return;
+		}
+		memset(data, 0, sizeof(data));
+		memcpy(data, (uint8_t[]){ 0x74 }, 1);
+		if (!cpld_i2c_write(I2C_BUS1, AEGIS_312M_CLK_GEN_ADDR, 0x14, data, 1)) {
+			LOG_ERR("Failed to write 312M CLK GEN");
+			return;
+		}
+		if (!cpld_i2c_read(I2C_BUS1, AEGIS_312M_CLK_GEN_ADDR, 0x14, data, 1)) {
+			LOG_ERR("Failed to read 312M CLK GEN");
+			return;
+		}
+
+	} else {
+		LOG_INF("pwrgd_p3v3_and_p1v8 fail= 0x%02X", pwrgd_p3v3_and_p1v8);
+	}
+}
+
+void plat_clock_init(void)
+{
+	LOG_DBG("plat_clock_init started");
+	uint8_t board_stage = get_board_stage();
+	if (board_stage == FAB2_DVT || board_stage == FAB3_PVT || board_stage == FAB4_MP) {
+		k_work_schedule(&check_clk_work, K_MSEC(100));
+	}
+}
+
+void plat_eusb_init(void)
+{
+	LOG_DBG("plat_eusb_init started");
+	uint8_t board_stage = get_board_stage();
+	if (board_stage == FAB2_DVT || board_stage == FAB3_PVT || board_stage == FAB4_MP) {
+		uint8_t data[1] = { 0 };
+		if (!cpld_i2c_read(I2C_BUS1, AEGIS_EUSB_REPEATER_ADDR, 0x05, data, 1)) {
+			LOG_ERR("Failed to read eUSB Repeater");
+			return;
+		}
+
+		uint8_t eusb_default_setting = data[0];
+		if (eusb_default_setting == 0x10) {
+			memset(data, 0, sizeof(data));
+			memcpy(data, (uint8_t[]){ 0x20 }, 1);
+			if (!cpld_i2c_write(I2C_BUS1, AEGIS_EUSB_REPEATER_ADDR, 0x05, data, 1)) {
+				LOG_ERR("Failed to write eUSB Repeater");
+				return;
+			}
+			if (!cpld_i2c_read(I2C_BUS1, AEGIS_EUSB_REPEATER_ADDR, 0x05, data, 1)) {
+				LOG_ERR("Failed to read eUSB Repeater");
+				return;
+			}
+		} else {
+			LOG_INF("eUSB Repeater addr=0x05 default seeting=0x%02X",
+				eusb_default_setting);
+		}
+	}
 }
